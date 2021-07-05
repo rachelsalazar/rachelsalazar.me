3b8957e597ad8b2e67e6201fac4f6d5e997e694e
Rachel Salazar <ret1992@gmail.com>
2021-07-04 21:24:42 -0700

    Fixed menu so if user clicks anywhere else on page it will close out of menu

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <div className="app">
+    <div className="app" menuOpen={menuOpen} setMenuOpen={setMenuOpen} onClick={() => menuOpen && setMenuOpen(false)}>
       <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
