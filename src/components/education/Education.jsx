55d363eff1096798b6136339badb52804039c7a6
Rachel Salazar <ret1992@gmail.com>
2021-07-09 15:45:37 -0700

    Added first container for education slides

--- src/components/education/Education.jsx
+++ src/components/education/Education.jsx
@@ -3,7 +3,16 @@ import "./education.scss";
 export default function Education() {
     return (
         <div className="education" id="education">
-            
+            <div className="slider">
+                <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <h1>Kal Academy</h1>
+                        </div>
+                        <div className="right"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
