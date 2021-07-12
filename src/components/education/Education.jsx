dc0539f43f818562fa3f7e4e5ec11ef2dfde02d6
Rachel Salazar <ret1992@gmail.com>
2021-07-12 10:56:29 -0700

    Added info to Kal Academy card in education section, tweaked styling in education and projects

--- src/components/education/Education.jsx
+++ src/components/education/Education.jsx
@@ -3,13 +3,28 @@ import "./education.scss";
 export default function Education() {
     return (
         <div className="education" id="education">
+            <h1>Education</h1>
             <div className="slider">
                 <div className="container">
                     <div className="item">
                         <div className="left">
-                            <h1>Kal Academy</h1>
+                            <div className="leftContainter">
+                                <div className="imgContainer">
+                                    <img src="assets/kal.png" alt="Kal Academy Logo"/>
+                                </div>
+                            </div>
                         </div>
-                        <div className="right"></div>
+                        <div className="right">
+                            <p>
+                                Kal Academy is dedicated to providing women and underrepresented minorities with the skills and tools they need to thrive in today’s tech industry.
+                                <br></br><br></br>1. Built web application using Angular and Typescript
+                                <br></br>2. Built real-world full stack application using React and MongoDb
+                                <br></br>3. Integrated app with React Router and Redux
+                                <br></br>4. Deployed application to cloud
+                                <br></br>5. Learned basic and advanced Algorithms and Data Structures
+                            </p>
+                            <button>Certificate</button>
+                        </div>
                     </div>
                 </div>
             </div>
