c0812f5eb66edf270a8555af0a10351b892ed576
Rachel Salazar <ret1992@gmail.com>
2021-07-14 12:19:46 -0700

    Added React Material UI Icons for social media links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,7 @@
 import "./navbar.scss"
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 export default function Navbar({ menuOpen, setMenuOpen }) {
     return (
@@ -8,9 +11,9 @@ export default function Navbar({ menuOpen, setMenuOpen }) {
                     <a href="#home" className="logo">hi. 👋🏼 </a>
                 </div>
                 <div class="icons">
-                    <a href="https://www.linkedin.com/in/rachel-e-salazar/" target="blank"><img src="../../assets/linkedin-white.png" alt="linkedin link"/></a>
-                    <a href="https://twitter.com/rach_e_l_" target="blank"><img src="../../assets/twitter-white.png" alt="twitter link"/></a>
-                    <a href="https://github.com/rachelsalazar" target="blank"><img src="../../assets/github-white.png" alt="github link"/></a>
+                    <a href="https://www.linkedin.com/in/rachel-e-salazar/" target="blank"><LinkedInIcon /></a>
+                    <a href="https://twitter.com/rach_e_l_" target="blank"><TwitterIcon /></a>
+                    <a href="https://github.com/rachelsalazar" target="blank"><GitHubIcon /></a>
                 </div>
                 <div className="right">
                     <span className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>🍔</span>
