308da2f0984a9a7f3158688823b6f56b86122fb9
Rachel Salazar <ret1992@gmail.com>
2021-07-14 08:38:52 -0700

    Added dynamic data for education slide

--- src/components/education/Education.jsx
+++ src/components/education/Education.jsx
@@ -1,35 +1,63 @@
+import { useState } from 'react';
 import "./education.scss";
 
 export default function Education() {
+
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    const data = [
+        {
+            id: '1',
+            image: 'assets/kal.png',
+            alt: 'Kal Academy Logo',
+            title: 'Full Stack Web Development Bootcamp',
+            desc: 'Kal Academy is dedicated to providing women and underrepresented minorities with the skills and tools they need to thrive in today’s tech industry. 1. Built web application using Angular and Typescript 2. Built real-world full stack application using React and MongoDb 3. Integrated app with React Router and Redux 4. Deployed application to cloud 5. Learned basic and advanced Algorithms and Data Structures'
+        },
+        {
+            id: '2',
+            image: 'assets/code-fellows.png',
+            alt: 'Code Fellows Logo',
+            title: 'Front End Web Development Bootcamp',
+            desc: 'Developed a well-rounded foundation of skills necessary for modern web development. These skills include HTML, CSS, and JavaScript coding; utilizing Git workflow processes, project organization, designing with wireframing and employing introductory Agile development methods.'
+        },
+        {
+            id: '3',
+            image: 'assets/london-app.png',
+            alt: 'London App Brewery Logo',
+            title: 'Full Stack Web Development Certificate',
+            desc: 'Topics Learned: Front-End Web Development, HTML 5, CSS 3, Bootstrap 4, Javascript ES6, DOM Manipulation, jQuery, Bash Command Line, Git, GitHub and Version Control, Backend Web Development, Node.js, NPM, Express.js, EJS, REST, APIs, Databases, SQL, MongoDB, Mongoose, Authentication, Firebase, React.js, React Hooks, Web Design, Deployment with GitHub Pages, Heroku and MongoDB Atlas'
+        },
+    ];
+
+    const handleClick = (direction) => {
+        direction === "left" ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2) :
+        setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+    }
+
     return (
         <div className="education" id="education">
             <h1>Education</h1>
-            <div className="slider">
-                <div className="container">
+            <div className="slider" style={{transform: `translateX(-${currentSlide * 100}vw)`}}>
+                {data.map((d) => (
+                    <div className="container">
                     <div className="item">
                         <div className="left">
                             <div className="leftContainter">
                                 <div className="imgContainer">
-                                    <img src="assets/kal.png" alt="Kal Academy Logo"/>
+                                    <img src={d.image} alt={d.alt}/>
                                 </div>
                             </div>
                         </div>
                         <div className="right">
-                            <p>
-                                Kal Academy is dedicated to providing women and underrepresented minorities with the skills and tools they need to thrive in today’s tech industry.
-                                <br></br><br></br>1. Built web application using Angular and Typescript
-                                <br></br>2. Built real-world full stack application using React and MongoDb
-                                <br></br>3. Integrated app with React Router and Redux
-                                <br></br>4. Deployed application to cloud
-                                <br></br>5. Learned basic and advanced Algorithms and Data Structures
-                            </p>
-                            <button>Certificate</button>
+                            <h3>{d.title}</h3>
+                            <p>{d.desc}</p>
                         </div>
                     </div>
                 </div>
+                ))}
             </div>
-            <img src="assets/left-arrow.png" className="arrow left" alt="Left arrow"/>
-            <img src="assets/right-arrow.png" className="arrow right" alt="Right arrow"/>
+            <img src="assets/left-arrow.png" className="arrow left" alt="Left arrow" onClick={() => handleClick("left")}/>
+            <img src="assets/right-arrow.png" className="arrow right" alt="Right arrow" onClick={() => handleClick("right")}/>
         </div>
     )
 }
